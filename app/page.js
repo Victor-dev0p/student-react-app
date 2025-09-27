7719449366df0ef9a5d181c599f3108424973bb0
victor erukpe <148998306+Victor-dev0p@users.noreply.github.com>
2025-09-27 17:56:02 +0100

    reinitailizing

--- app/page.js
+++ app/page.js
@@ -1,7 +1,28 @@
-export default function Home() {
-  return (
-   <div>
-    <p>This is Our First React App </p>
-   </div>
-  );
+"use client"
+import { useContext, useState } from "react"
+// import Welcome from "@/components/explore";
+import { ThemeContext } from "@/utils/ThemeContext";
+import { FaUsers } from "react-icons/fa";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const theme = useContext(ThemeContext);
+  
+  return(
+    <div style = {{background: theme === 'light' ? 'fff' : '#333', color: theme === 'light' ? '#0000': 'ffff'}}>
+      <p>You clicked {count} times</p>
+      <button onClick={()=>setCount(count +1)}>Click me <FaUsers /></button>
+    </div> 
+  )
+  
 }
+
+function ThemeWrapper () {
+  return (
+    <ThemeContext.Provider value="">
+      <App />
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeWrapper
---
+++ components/explore.js
@@ -0,0 +1,6 @@
+function Welcome (props) {
+  return (
+    <h1>Welcome and Hello, {props.student}</h1>
+  )
+}
+export default Welcome;
